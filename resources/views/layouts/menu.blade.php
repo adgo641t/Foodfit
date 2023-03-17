154b40f562dd7c70740244e41da14e313db41b92
wintop910 <wintopadedokun@gmail.com>
2023-03-17 09:28:03 +0100

    Fixed menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -54,6 +54,9 @@
                                 aria-expanded="false" aria-label="Toggle navigation">
                                 <span class="navbar-toggler-icon"></span>
                             </button>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('/abouts') }}">About</a>
+                            </li>
                                 <!--If user is logged-->
                                 @if(@Auth::user()->hasRole('cliente'))
                                 <li class="nav-item">
@@ -63,6 +66,16 @@
                                 <li class="nav-item">
                                     <a class="nav-link" aria-current="page" href="{{ url('/blog') }}">Blog</a>
                                 </li>
+                                                                                <!--Icon cart and go to cart-->
+                                                                                <a href="{{ route('cart.list') }}" class="flex items-center">
+                                                                                    <svg class="w-5 h-5" fill="none" stroke-linecap="round" stroke-linejoin="round"
+                                                                                        stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                                                                                        <path
+                                                                                            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z">
+                                                                                        </path>
+                                                                                    </svg>
+                                                                                    {{ Cart::getTotalQuantity() }}
+                                                                                </a>
                                 @endif
                                 @if(@Auth::user()->hasRole('admin'))
                                 <li class="nav-item">
@@ -75,16 +88,6 @@
                              <!--To logout-->
                              <li class="nav-item">
                             <a href="{{ url('/logout') }}"class="nav-link">Logout</a></li>
-                                                <!--Icon cart and go to cart-->
-                                                <a href="{{ route('cart.list') }}" class="flex items-center">
-                                                    <svg class="w-5 h-5" fill="none" stroke-linecap="round" stroke-linejoin="round"
-                                                        stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                                                        <path
-                                                            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z">
-                                                        </path>
-                                                    </svg>
-                                                    {{ Cart::getTotalQuantity() }}
-                                                </a>
                             @else
                             <a href="{{ url('/') }}"><img src="logo.png" alt="" class="logo-img"></a>
                             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
