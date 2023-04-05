e8bfd74226e199cda5437d36c3c6f05e8810d834
adgo0641t <fpb1.adrian@gmail.com>
2023-04-05 09:14:30 +0200

    ROl chef

--- database/seeders/CreateUsersSeeder.php
+++ database/seeders/CreateUsersSeeder.php
@@ -19,7 +19,7 @@ class CreateUsersSeeder extends Seeder
     public function run()
     {
 
-        
+        //Rol de Administrador
         $user = User::create([
             'name' => 'Adrian',
             'email'     => 'Adrian@UPC.com',
@@ -58,6 +58,7 @@ class CreateUsersSeeder extends Seeder
         $role->givePermissionTo('UpdateCoupons');
 
 
+        //Rol de Blog Creator
         $user_blog = User::create([
             'name' => 'Jose',
             'email'     => 'Jose@upc.com',
@@ -81,8 +82,16 @@ class CreateUsersSeeder extends Seeder
 
         $user_cliente->assignRole($role_cliente);
 
+        //Rol de Chef
+        $user_cliente= User::create([
+            'name' => 'Chef',
+            'email'     => 'Chef@upc.com',
+            'password' => '$2y$10$x.Ot5fO84DkomAhWkPBXTOEYNjsn.pZ9YGS2PDFPpfve8eJYsKu0q',
+        ]);
 
-        
+        $role_cliente = Role::create(['name' => 'chef']);
+
+        $role_Blog->givePermissionTo('UpdatePedido');        
         // $users = [
         //     [
         //        'name'=>'Admin User',
